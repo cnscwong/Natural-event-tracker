2f9329da1d550002d38adb313f44fb58236a9616
cnscwong <105528854+cnscwong@users.noreply.github.com>
2022-08-20 14:16:18 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <header className="header">
-      <h1>Natural Event Tracker (Powered by NASA)</h1>
+      <h1>Natural Event Tracker</h1>
     </header>
   );
 };
